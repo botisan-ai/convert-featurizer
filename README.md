c811a57992e401364ea1dd96f8196c2f512e2413
Simon Liang <no.lhr0909@gmail.com>
2022-08-25 18:08:29 +0800

    added hub manifest

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# convert-featurizer
-PolyAI's ConveRT model packaged in Jina
+# ConveRT Featurizer
+
+PolyAI's ConveRT model packaged in Jina. Currently it is using [David Alami's packaged version](https://github.com/davidalami/ConveRT).
+
+
+## How to use
+
+Just pass in the sentences (one per document) and the model will return the features.
+
+## Author
+
+[Simon Liang](https://github.com/lhr0909)
+
+## License
+
+[MIT](./LICENSE)
--- executor.py
+++ executor.py
@@ -3,9 +3,9 @@ from jina import Executor, requests
 from docarray import DocumentArray
 
 class ConveRTFeaturizer(Executor):
-    def __init__(self, **kwargs):
+    def __init__(self, multiple_contexts=False, **kwargs):
         super(ConveRTFeaturizer, self).__init__(**kwargs)
-        self.sentence_encoder = SentenceEncoder(multiple_contexts=False)
+        self.sentence_encoder = SentenceEncoder(multiple_contexts=multiple_contexts)
 
     @requests
     def featurize(self, docs: DocumentArray, **kwargs) -> DocumentArray:
